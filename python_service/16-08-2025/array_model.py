11c8aabacf526751a7b9b3105ee82ccc39a932db
Vamsi <chmnvkrishna@gmail.com>
2025-08-17 19:22:30 +0530

    Initial commit

--- python_service/16-08-2025/array_model.py
+++ python_service/16-08-2025/array_model.py
@@ -3,10 +3,11 @@ import array
 numbers = array.array('i', [10, 20, 30, 40, 50])
 
 print(numbers[2]) 
+print(" ") 
 
 numbers.append(60)
 
-numbers.remove(20)
+numbers.remove(20) 
 
 for num in numbers:
     print(num)
