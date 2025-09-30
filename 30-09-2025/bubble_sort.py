339403d7bedd2562670aff95c21454184080a67b
Vamsi <chmnvkrishna@gmail.com>
2025-09-30 18:57:34 +0530

    Initial commit

---
+++ 30-09-2025/bubble_sort.py
@@ -0,0 +1,6 @@
+nums = [5, 3, 8, 6, 2]
+for i in range(len(nums)):
+    for j in range(0, len(nums)-i-1):
+        if nums[j] > nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+print(nums)
